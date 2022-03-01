3f345585aeb7996cfbf58ea852d99a2dcafcd10f
darker <darker@txl.com>
2022-03-01 17:13:13 +0800

    Idea第一次提交

---
+++ src/main/java/com/txl/git/TestGit.java
@@ -0,0 +1,13 @@
+package com.txl.git;
+
+/**
+ * 从零走向架构师
+ *
+ * @author 听雪楼~DARKER
+ * 既来之则安之
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("idea集成git");
+    }
+}
---
+++ src/test/java/com/txl/HelloGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.txl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
